62c383d0fabcc826800d2cb6d79dec888a417dda
bchartier <benjamin.chartier@cegetel.net>
2014-08-28 11:55:51 +0200

    Gestion du double-clic sur un style

--- plugin/geopicardie/plugin.py
+++ plugin/geopicardie/plugin.py
@@ -24,10 +24,11 @@ class FavoritesTreeNode:
   """
 
   def __init__(self, title, node_type=GpicNodeTypes.Instance().NODE_TYPE_FOLDER,
-    description=None, params=None):
+    description=None, params=None, parent_node=None):
     """
     """
 
+    self.parent_node = parent_node
     self.node_type = node_type
     self.title = title
     self.description = description
@@ -38,7 +39,10 @@ class FavoritesTreeNode:
       self.layer_name = params.get("name")
       self.layer_format = params.get("format")
       self.layer_srs = params.get("srs")
-      self.layer_style = params.get("style", "")
+      self.layer_style_name = params.get("style", "")
+
+    elif self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WMS_LAYER_STYLE:
+      self.layer_style_name = params.get("name")
 
     elif self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WFS_FEATURE_TYPE:
       self.service_url = params.get("url")
@@ -46,23 +50,51 @@ class FavoritesTreeNode:
       self.wfs_version = params.get("version", "1.0.0")
       self.layer_srs = params.get("srs")
 
+
   def runAction(self):
+    """
+    """
+
     if self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WMS_LAYER:
       self.addWMSLayer()
+    elif self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WMS_LAYER_STYLE:
+      self.addWMSLayerWithStyle()
     elif self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WMTS_LAYER:
       self.addWMTSLayer()
     elif self.node_type == GpicNodeTypes.Instance().NODE_TYPE_WFS_FEATURE_TYPE:
       self.addWFSLayer()
 
+
   def addWMSLayer(self):
+    """
+    """
+
     layer_url = u"crs={}&featureCount=10&format={}&layers={}&maxHeight=256&maxWidth=256&styles={}&url={}".format(
-      self.layer_srs, self.layer_format, self.layer_name, self.layer_style, self.service_url)
-    plugin_globals.iface.addRasterLayer(layer_url, self.layer_name, "wms")
+      self.layer_srs, self.layer_format, self.layer_name, self.layer_style_name, self.service_url)
+    plugin_globals.iface.addRasterLayer(layer_url, self.title, "wms")
+
+
+  def addWMSLayerWithStyle(self):
+    """
+    """
+
+    if self.parent_node != None:
+      layer_url = u"crs={}&featureCount=10&format={}&layers={}&maxHeight=256&maxWidth=256&styles={}&url={}".format(
+        self.parent_node.layer_srs, self.parent_node.layer_format, self.parent_node.layer_name, self.layer_style_name, self.parent_node.service_url)
+      plugin_globals.iface.addRasterLayer(layer_url, self.parent_node.title, "wms")
+
 
   def addWMTSLayer(self):
+    """
+    """
+
     pass
 
+
   def addWFSLayer(self):
+    """
+    """
+
     first_param_prefix = '?'
     if '?' in self.service_url:
       first_param_prefix = '&'
@@ -70,13 +102,16 @@ class FavoritesTreeNode:
       self.service_url, first_param_prefix, self.wfs_version, self.feature_type_name, self.layer_srs)
     plugin_globals.iface.addVectorLayer(layer_url, self.title, "WFS")
 
+
   def __str__(self):
     result = u"{} (description: {}, type: {}, children: {})".format(self.title, self.description, self.node_type, len(self.children))
     return
 
+
   def __unicode__(self):
     result = u"{} (description: {}, type: {}, children: {})".format(self.title, self.description, self.node_type, len(self.children))
     return
+
 
   def __repr__(self):
     result = u"{} (description: {}, type: {}, children: {})".format(self.title, self.description, self.node_type, len(self.children))
@@ -88,7 +123,7 @@ class FavoriteTreeNodeFactory:
   Class used to build FavoritesTreeNode instances
   """
 
-  def build_tree(self, tree_config):
+  def build_tree(self, tree_config, parent_node = None):
     """
     Function that do the job
     """
@@ -101,13 +136,13 @@ class FavoriteTreeNodeFactory:
 
     if node_title:
       # Creation of the node
-      node = FavoritesTreeNode(node_title, node_type, node_description, node_params)
+      node = FavoritesTreeNode(node_title, node_type, node_description, node_params, parent_node)
 
       # Creation of the node children
       node_children = tree_config.get('children', [])
       if len(node_children) > 0:
         for child_config in node_children:
-          child_node = self.build_tree(child_config)
+          child_node = self.build_tree(child_config, node)
           node.children.append(child_node)
 
       return node
